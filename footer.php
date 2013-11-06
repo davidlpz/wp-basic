dcaa20360c3d7b67a0d7d56bffb524548d2d2fb0
davidlpz <davidlpzgomez@gmail.com>
2013-11-06 09:14:16 +0100

    Analytics code in js file.

--- footer.php
+++ footer.php
@@ -1,11 +1,5 @@
 	<footer class="footer" role="contentinfo"></footer>
 </div>
 <?php wp_footer(); ?>
-<script>
-	var _gaq=[['_setAccount','UA-XXXXXXXX-XX'],['_trackPageview']];
-	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-	g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-	s.parentNode.insertBefore(g,s)})(document,'script');
-</script>
 </body>
 </html>
